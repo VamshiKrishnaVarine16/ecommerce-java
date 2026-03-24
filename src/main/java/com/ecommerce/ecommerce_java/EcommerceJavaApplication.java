d9a35fd7e88f0109e1323d330940783f3fbb52b4
VamshiKrishnaVarine16 <varinevamshikrishna@gmail.com>
2026-03-24 11:43:58 +0530

    Add CORS configuration for frontend

--- src/main/java/com/ecommerce/ecommerce_java/EcommerceJavaApplication.java
+++ src/main/java/com/ecommerce/ecommerce_java/EcommerceJavaApplication.java
@@ -2,6 +2,9 @@ package com.ecommerce.ecommerce_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EcommerceJavaApplication {
@@ -10,4 +13,17 @@ public class EcommerceJavaApplication {
 		SpringApplication.run(EcommerceJavaApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
